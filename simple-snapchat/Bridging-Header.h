9b61e30f5211249265a12eea309a896a28fc1472
Lucas Blancas <lablancas@gmail.com>
2017-06-21 13:09:25 -0500

    import and configure GeoFire

---
+++ simple-snapchat/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  simple-snapchat
+//
+//  Created by Lucas Blancas on 6/21/17.
+//  Copyright © 2017 University of Melbourne. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "GeoFire.h"
+
+#endif /* Bridging_Header_h */
